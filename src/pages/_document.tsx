ce55755be5bb0bb29446a54f96240529e68349a5
Parsa Ghadimkhani <parsa.ghadimkhani@webjar.ir>
2023-05-23 19:24:27 +0330

    feat(global): add direction to html based on locale

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,8 +1,10 @@
+import type { DocumentContext } from 'next/document';
 import { Html, Head, Main, NextScript } from 'next/document';
+import i18n from 'i18next';
 
-export default function Document() {
+export default function Document({ locale }: DocumentContext) {
   return (
-    <Html lang="en">
+    <Html dir={i18n.dir(locale)}>
       <Head />
       <body>
         <Main />
