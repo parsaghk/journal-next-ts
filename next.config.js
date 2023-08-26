8715fd1141fbb5b6ddd45915855243f3b8fd5efd
Parsa Ghadimkhani <parsa.ghadimkhani@webjar.ir>
2023-08-26 20:19:02 +0330

    feat: rewrite api base url

--- next.config.js
+++ next.config.js
@@ -4,6 +4,14 @@ const { i18n } = require('./next-i18next.config');
 const nextConfig = {
   i18n,
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${process.env.NEXT_PUBLIC_API_BASE_URL}/:path*`,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
