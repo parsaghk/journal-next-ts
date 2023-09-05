333ad45a64c5bca1be0c3439bcc97e8f00d6ab1f
Parsa Ghadimkhani <parsa.ghadimkhani@webjar.ir>
2023-09-05 12:45:41 +0330

    feat: add next query and antd locale

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,11 +6,18 @@ import {
   QueryClientProvider,
 } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { ConfigProvider } from 'antd';
+import { ConfigProvider, notification } from 'antd';
+import { Locale } from 'antd/lib/locale';
+import enUS from 'antd/locale/en_US';
+import faIR from 'antd/locale/fa_IR';
+import frFR from 'antd/locale/fr_FR';
 import { NextPage } from 'next';
 import { AppProps } from 'next/app';
-import { appWithTranslation } from 'next-i18next';
+import { appWithTranslation, i18n } from 'next-i18next';
+import NextAdapterPages from 'next-query-params/pages';
+import queryString from 'query-string';
 import React from 'react';
+import { QueryParamProvider } from 'use-query-params';
 
 export type NextPageWithLayout<P = object, IP = P> = NextPage<P, IP> & {
   getLayout?: (props: TDashboardLayout) => React.JSX.Element;
@@ -20,8 +27,30 @@ type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayout;
 };
 
-function App({ Component, pageProps }: AppPropsWithLayout) {
-  const [queryClient] = React.useState(() => new QueryClient());
+const appLocaleToAntDesignLocaleMapper = new Map<string, Locale>([
+  ['fa', faIR],
+  ['en', enUS],
+  ['fr', frFR],
+]);
+
+function App({ Component, pageProps, router }: AppPropsWithLayout) {
+  const [queryClient] = React.useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          mutations: {
+            onSuccess: () => {
+              notification.success({
+                message: 'Operation completed successfully',
+                placement: 'bottomRight',
+              });
+              router.back();
+            },
+          },
+        },
+      })
+  );
+
   const PageLayout = Component.getLayout;
   const Page = () =>
     PageLayout ? (
@@ -35,8 +64,19 @@ function App({ Component, pageProps }: AppPropsWithLayout) {
   return (
     <QueryClientProvider client={queryClient}>
       <Hydrate state={pageProps.dehydratedState}>
-        <ConfigProvider>
-          <Page />
+        <ConfigProvider
+          direction={i18n?.dir(router.locale)}
+          locale={appLocaleToAntDesignLocaleMapper.get(router.locale as string)}
+        >
+          <QueryParamProvider
+            adapter={NextAdapterPages}
+            options={{
+              searchStringToObject: queryString.parse,
+              objectToSearchString: queryString.stringify,
+            }}
+          >
+            <Page />
+          </QueryParamProvider>
         </ConfigProvider>
       </Hydrate>
       <ReactQueryDevtools initialIsOpen={false} />
