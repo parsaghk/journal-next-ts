6041ae03cbd37daf005192fdf3283e38d0b921e3
Parsa Ghadimkhani <parsa.ghadimkhani@webjar.ir>
2023-09-05 12:41:58 +0330

    feat(utils): add authorization header to axios

--- src/utils/axios.util.ts
+++ src/utils/axios.util.ts
@@ -1,5 +1,11 @@
+import { useTokenStore } from '@store/use-token';
 import Axios from 'axios';
+
+const { accessToken } = useTokenStore.getState();
 
 export const axios = Axios.create({
   baseURL: '/api',
+  headers: {
+    ...(accessToken && { Authorization: `Bearer ${accessToken}` }),
+  },
 });
