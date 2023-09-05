30173af6220b1e8f3b2f6e73df30585a0e8bbfb3
Parsa Ghadimkhani <parsa.ghadimkhani@webjar.ir>
2023-09-05 13:00:42 +0330

    feat(shared): implement custom use query options hook

---
+++ src/shared/hooks/use-query-options.hook.ts
@@ -0,0 +1,48 @@
+import { TPlainLiteralObject } from '@shared/types';
+import flat, { unflatten } from 'flat';
+import _ from 'lodash';
+import { useRouter } from 'next/router';
+import qs from 'qs';
+
+export function useQueryOptions<
+  T extends TPlainLiteralObject = TPlainLiteralObject,
+>(defaultQueryOptions?: TPlainLiteralObject): T {
+  const router = useRouter();
+  const currentQueryOptions: TPlainLiteralObject = (qs.parse(
+    router.asPath.substring(router.asPath.lastIndexOf('?'))
+  ) ?? {}) as TPlainLiteralObject;
+  const mergedQueryOptions = mergeQueryOptions(
+    currentQueryOptions,
+    defaultQueryOptions
+  );
+  if (isQueryOptionsChanged(currentQueryOptions, mergedQueryOptions)) {
+    router.push(router.asPath, { query: qs.stringify(mergedQueryOptions) });
+  }
+  return mergedQueryOptions as T;
+}
+
+function mergeQueryOptions(
+  currentQueryOptions: TPlainLiteralObject = {},
+  defaultQueryOptions: TPlainLiteralObject = {}
+): TPlainLiteralObject {
+  const flattenCurrentQueryOptions: TPlainLiteralObject =
+    flat(currentQueryOptions);
+  const flattenDefaultQueryOptions: TPlainLiteralObject =
+    flat(defaultQueryOptions);
+  const flattenMergedQueryOptions: TPlainLiteralObject = {
+    ...flattenCurrentQueryOptions,
+    ...flattenDefaultQueryOptions,
+  };
+  return unflatten(flattenMergedQueryOptions);
+}
+
+function isQueryOptionsChanged(
+  previousQueryOptions: TPlainLiteralObject,
+  currentQueryOptions: TPlainLiteralObject
+) {
+  return !_.isEqual(
+    _.omitBy(previousQueryOptions, _.isEmpty),
+    _.omitBy(currentQueryOptions, _.isEmpty)
+  );
+  return false;
+}
