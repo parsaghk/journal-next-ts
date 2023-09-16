13b36706165837d40b4b0e95c331f9f99cf630aa
Parsa Ghadimkhani <parsa.ghadimkhani@webjar.ir>
2023-09-16 21:39:38 +0330

    feat(components): implement badge component

---
+++ src/components/Badge/index.ts
@@ -0,0 +1,4 @@
+import Badge from './badge.component';
+
+export default Badge;
+export * from './types';
---
+++ src/components/Badge/types/badge-props.type.ts
@@ -0,0 +1,12 @@
+export type TBadgeProps = {
+  text: string;
+  color:
+    | 'gray'
+    | 'red'
+    | 'yellow'
+    | 'green'
+    | 'blue'
+    | 'indigo'
+    | 'purple'
+    | 'pink';
+};
