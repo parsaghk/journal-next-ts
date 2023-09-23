78c843c6d5ad8a842308485f6033190f87fe6d3d
Parsa Ghadimkhani <parsa.ghadimkhani@webjar.ir>
2023-09-23 15:46:34 +0330

    feat(assets,styles): add vazir font

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,9 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      Vazir: ['Vazir'],
+    },
   },
   plugins: [],
   corePlugins: {
