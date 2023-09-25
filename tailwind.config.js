84e94fd735a160573404ff76985988cf62bf8feb
Parsa Ghadimkhani <parsa.ghadimkhani@webjar.ir>
2023-09-25 19:59:10 +0330

    feat: add gridTemplateColumns

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,15 @@ module.exports = {
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/layouts/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     fontFamily: {
       Vazir: ['Vazir'],
+    },
+    extend: {
+      gridTemplateColumns: {
+        24: 'repeat(24, minmax(0, 1fr))',
+      },
     },
   },
   plugins: [],
